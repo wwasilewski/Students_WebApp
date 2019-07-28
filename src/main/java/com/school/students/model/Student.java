44eef57f5695035b0ef4862f1501b99c7394218f
wwasilewski <w.wasilewski@gmail.com>
2019-07-28 13:35:57 +0200

    added Student class

---
+++ src/main/java/com/school/students/model/Student.java
@@ -0,0 +1,29 @@
+package com.school.students.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * @author Wojciech Wasilewski
+ * @date Created on 28.07.2019
+ */
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String department;
+    private String email;
+}
